776ce8c8d3475958a29b548f81616b3a2a413622
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-05-24 21:16:35 +0300

    Edit Footer Links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,38 +1,50 @@
+import Link from 'next/link';
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const footerLinks = [
   {
     title: "YouTube",
     link: "https://www.youtube.com/@AhmedRFRF-z3u",
+    isExternal: true,
   },
   {
     title: "Twitter",
     link: "https://x.com/AHMED76865307",
+    isExternal: true,
   },
   {
     title: "Instagram",
     link: "https://www.instagram.com/ahmed7_707_7/",
+    isExternal: true,
   },
   {
     title: "LinkedIn",
     link: "https://www.linkedin.com/in/ahmed-saad-833819366/",
+    isExternal: true,
   },
 ];
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative z-10 overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
           <div>
             <nav className="flex flex-col items-center gap-8 md:flex-row">
-              {footerLinks.map((link => (
-                <a key={link.title} href={link.link} className="inline-flex items-center gap-1.5">
-                  <span className="font-semibold">{link.title}</span>
-                  <ArrowUpRightIcon className="size-4" />
-                </a>
-              )))}
+              {footerLinks.map(link => (
+                link.isExternal ? (
+                  <a key={link.title} href={link.link} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-1.5">
+                    <span className="font-semibold">{link.title}</span>
+                    <ArrowUpRightIcon className="size-4" />
+                  </a>
+                ) : (
+                  <Link key={link.title} href={link.link} className="inline-flex items-center gap-1.5">
+                    <span className="font-semibold">{link.title}</span>
+                    <ArrowUpRightIcon className="size-4" />
+                  </Link>
+                )
+              ))}
             </nav>
           </div>
           <div className="text-white/40">&copy; 2025 Ahmed Alrefaey. All rights reserved.</div>
