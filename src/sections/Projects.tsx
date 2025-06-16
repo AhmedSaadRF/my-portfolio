ea619745c81d47369c404046e826037507313016
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-16 09:59:37 +0300

    Upload Project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -50,7 +50,7 @@ const portfolioProjects = [
   {
     company: "RFRF Projects",
     year: "2025",
-    title: "To-Do-App Project",
+    title: "To-Do-App-List Project",
     results: [
       { title: "React"},
       { title: "Java Script" },
