4b41719064e2f92ec9049a47a7a5e4c34e1cf1ab
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-27 16:54:57 +0300

    Upload Project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -63,7 +63,7 @@ const portfolioProjects = [
   {
     company: "RFRF Projects",
     year: "2025",
-    title: "AI SEO Landing<br>Page",
+    title: "AI SEO Landing Page",
     results: [
       { title: "Next js" },
       { title: "Tailwind CSS" },
