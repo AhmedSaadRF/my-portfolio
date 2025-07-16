684b460cda0022ce48de5796dac3c0b6fbdb0892
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-16 09:35:18 +0300

    Upload Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"   />
       </head>
       <body className={twMerge(inter.variable , calistoga.variable , "bg-gray-900 text-white antialiased font-sans")}>{children}</body>
     </html>
