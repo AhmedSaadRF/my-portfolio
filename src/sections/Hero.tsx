090ad1d6c50cf1182765973372a2405f6207e4dc
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-08-12 12:40:10 +0300

    Upload Project

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,6 +6,7 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 import Image from "next/image";
 import { HeroOrbit } from "@/components/HeroOrbit";
 import whatsappImg from "@/assets/images/icons8-whatsapp-48.png";
+import DownloadCVButton from "@/components/DownloadCV.Button";
 
 export const HeroSection = () => {
   return (
@@ -68,10 +69,7 @@ export const HeroSection = () => {
 
         <div className="flex flex-col items-center mt-8 gap-2 w-full">
           <div className="flex flex-row justify-center items-center gap-4 w-full">
-            <a href="" target="_blank" className="inline-flex items-center gap-2 border border-white/15 px-5 h-12 rounded-xl bg-emerald-500 hover:bg-emerald-600 text-white font-semibold shadow transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-emerald-400">
-              <span className="font-semibold">Download CV</span>
-              <svg xmlns="http://www.w3.org/2000/svg" className="size-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V4" /></svg>
-            </a>
+            <DownloadCVButton />
             <a href="https://wa.me/+201063596560" target="_blank" className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl font-semibold shadow hover:bg-emerald-50 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-emerald-400">
               <Image src={whatsappImg} alt="Whatsapp" className="size-7" />
               <span className="font-semibold">Let&apos;s Connect</span>
